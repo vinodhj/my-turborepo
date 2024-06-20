803c108ac0c129d0367673736fcd0ff9619dff12
Vinodh <vinodh.jeevanantham@gmail.com>
2024-06-20 20:41:42 +0530

    graphql yoga setup

--- services/cf-worker/src/index.ts
+++ services/cf-worker/src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { YogaSchemaDefinition, createYoga } from 'graphql-yoga';
+import { CfWorkersDataSource } from './datasources';
+import { schema } from './schemas';
+
+export interface YogaInitialContext {
+	datasources: {
+		cfWorkersDataSource: CfWorkersDataSource;
+	};
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env) {
+		const url = new URL(request.url);
+		const datasources = {
+			cfWorkersDataSource: new CfWorkersDataSource(),
+		};
+		const yoga = createYoga({
+			schema: schema as YogaSchemaDefinition<object, YogaInitialContext>,
+			landingPage: false,
+			graphqlEndpoint: '/graphql',
+			context: () => ({
+				datasources: {
+					cfWorkersDataSource: new CfWorkersDataSource(),
+				},
+			}),
+		});
+
+		return yoga.fetch(request);
 	},
-} satisfies ExportedHandler<Env>;
+};
