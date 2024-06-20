7373c1242f6f8e66171d52b08ef4c2428b5c1182
Vinodh <vinodh.jeevanantham@gmail.com>
2024-06-20 20:50:42 +0530

    package updated

--- services/cf-worker/src/index.ts
+++ services/cf-worker/src/index.ts
@@ -3,28 +3,28 @@ import { CfWorkersDataSource } from './datasources';
 import { schema } from './schemas';
 
 export interface YogaInitialContext {
-	datasources: {
-		cfWorkersDataSource: CfWorkersDataSource;
-	};
+  datasources: {
+    cfWorkersDataSource: CfWorkersDataSource;
+  };
 }
 
 export default {
-	async fetch(request: Request, env: Env) {
-		const url = new URL(request.url);
-		const datasources = {
-			cfWorkersDataSource: new CfWorkersDataSource(),
-		};
-		const yoga = createYoga({
-			schema: schema as YogaSchemaDefinition<object, YogaInitialContext>,
-			landingPage: false,
-			graphqlEndpoint: '/graphql',
-			context: () => ({
-				datasources: {
-					cfWorkersDataSource: new CfWorkersDataSource(),
-				},
-			}),
-		});
+  async fetch(request: Request, env: Env) {
+    const url = new URL(request.url);
+    const datasources = {
+      cfWorkersDataSource: new CfWorkersDataSource(),
+    };
+    const yoga = createYoga({
+      schema: schema as YogaSchemaDefinition<object, YogaInitialContext>,
+      landingPage: false,
+      graphqlEndpoint: '/graphql',
+      context: () => ({
+        datasources: {
+          cfWorkersDataSource: new CfWorkersDataSource(),
+        },
+      }),
+    });
 
-		return yoga.fetch(request);
-	},
+    return yoga.fetch(request);
+  },
 };
