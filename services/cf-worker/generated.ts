9632f4acf999383e221992da1e1b556d7f88d75b
Vinodh <vinodh.jeevanantham@gmail.com>
2024-06-26 18:41:05 +0530

    types updated

--- services/cf-worker/generated.ts
+++ services/cf-worker/generated.ts
@@ -1,4 +1,4 @@
-import { GraphQLResolveInfo } from 'graphql';
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -6,6 +6,7 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string };
@@ -13,11 +14,62 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean };
   Int: { input: number; output: number };
   Float: { input: number; output: number };
+  DateTime: { input: any; output: any };
+  JSON: { input: any; output: any };
+};
+
+export type CreateServiceInput = {
+  description?: InputMaybe<Scalars['String']['input']>;
+  name: Scalars['String']['input'];
+};
+
+export type Enquiry = {
+  __typename?: 'Enquiry';
+  created_at: Scalars['DateTime']['output'];
+  email: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  message?: Maybe<Scalars['String']['output']>;
+  selected_services: Array<Service>;
+  updated_at: Scalars['DateTime']['output'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createService: Service;
+  submitEnquiry: Enquiry;
+};
+
+export type MutationCreateServiceArgs = {
+  input?: InputMaybe<CreateServiceInput>;
+};
+
+export type MutationSubmitEnquiryArgs = {
+  input?: InputMaybe<SubmitEnquiryInput>;
 };
 
 export type Query = {
   __typename?: 'Query';
-  hello?: Maybe<Scalars['String']['output']>;
+  enquiry?: Maybe<Enquiry>;
+};
+
+export type QueryEnquiryArgs = {
+  id: Scalars['ID']['input'];
+};
+
+export type Service = {
+  __typename?: 'Service';
+  created_at: Scalars['DateTime']['output'];
+  description?: Maybe<Scalars['String']['output']>;
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  updated_at: Scalars['DateTime']['output'];
+};
+
+export type SubmitEnquiryInput = {
+  email: Scalars['String']['input'];
+  json_text: Scalars['JSON']['input'];
+  message?: InputMaybe<Scalars['String']['input']>;
+  selected_services: Scalars['JSON']['input'];
 };
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
@@ -89,15 +141,31 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
+  CreateServiceInput: CreateServiceInput;
+  DateTime: ResolverTypeWrapper<Scalars['DateTime']['output']>;
+  Enquiry: ResolverTypeWrapper<Enquiry>;
+  ID: ResolverTypeWrapper<Scalars['ID']['output']>;
+  JSON: ResolverTypeWrapper<Scalars['JSON']['output']>;
+  Mutation: ResolverTypeWrapper<{}>;
   Query: ResolverTypeWrapper<{}>;
+  Service: ResolverTypeWrapper<Service>;
   String: ResolverTypeWrapper<Scalars['String']['output']>;
+  SubmitEnquiryInput: SubmitEnquiryInput;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
   Boolean: Scalars['Boolean']['output'];
+  CreateServiceInput: CreateServiceInput;
+  DateTime: Scalars['DateTime']['output'];
+  Enquiry: Enquiry;
+  ID: Scalars['ID']['output'];
+  JSON: Scalars['JSON']['output'];
+  Mutation: {};
   Query: {};
+  Service: Service;
   String: Scalars['String']['output'];
+  SubmitEnquiryInput: SubmitEnquiryInput;
 };
 
 export type OneOfDirectiveArgs = {};
@@ -109,12 +177,49 @@ export type OneOfDirectiveResolver<Result, Parent, ContextType = any, Args = One
   Args
 >;
 
+export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
+  name: 'DateTime';
+}
+
+export type EnquiryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Enquiry'] = ResolversParentTypes['Enquiry']> = {
+  created_at?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  message?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  selected_services?: Resolver<Array<ResolversTypes['Service']>, ParentType, ContextType>;
+  updated_at?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export interface JsonScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['JSON'], any> {
+  name: 'JSON';
+}
+
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  createService?: Resolver<ResolversTypes['Service'], ParentType, ContextType, Partial<MutationCreateServiceArgs>>;
+  submitEnquiry?: Resolver<ResolversTypes['Enquiry'], ParentType, ContextType, Partial<MutationSubmitEnquiryArgs>>;
+};
+
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
-  hello?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  enquiry?: Resolver<Maybe<ResolversTypes['Enquiry']>, ParentType, ContextType, RequireFields<QueryEnquiryArgs, 'id'>>;
+};
+
+export type ServiceResolvers<ContextType = any, ParentType extends ResolversParentTypes['Service'] = ResolversParentTypes['Service']> = {
+  created_at?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  description?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  updated_at?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
 export type Resolvers<ContextType = any> = {
+  DateTime?: GraphQLScalarType;
+  Enquiry?: EnquiryResolvers<ContextType>;
+  JSON?: GraphQLScalarType;
+  Mutation?: MutationResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
+  Service?: ServiceResolvers<ContextType>;
 };
 
 export type DirectiveResolvers<ContextType = any> = {
--- services/cf-worker/src/types/index.ts
+++ services/cf-worker/src/types/index.ts
@@ -1,7 +1,44 @@
 import { gql } from 'graphql-tag';
 
 export const typeDefs = gql`
+  scalar JSON
+  scalar DateTime
+
+  type Service {
+    id: ID!
+    name: String!
+    description: String
+    created_at: DateTime!
+    updated_at: DateTime!
+  }
+
+  type Enquiry {
+    id: ID! #nano_id
+    email: String!
+    message: String
+    selected_services: [Service!]!
+    created_at: DateTime!
+    updated_at: DateTime!
+  }
+
+  input SubmitEnquiryInput {
+    email: String!
+    message: String
+    selected_services: JSON! # services IDS
+    json_text: JSON!
+  }
+
+  input CreateServiceInput {
+    name: String!
+    description: String
+  }
+
   type Query {
-    hello: String
+    enquiry(id: ID!): Enquiry
+  }
+
+  type Mutation {
+    submitEnquiry(input: SubmitEnquiryInput): Enquiry!
+    createService(input: CreateServiceInput): Service!
   }
 `;
